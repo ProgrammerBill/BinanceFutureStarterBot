ba7ae28446cf596844d133d2220923507a02fd7d
bill <cjcbill@gmail.com>
2024-01-30 17:03:45 +0800

    feat: daily update.

--- futures_bot_runner.py
+++ futures_bot_runner.py
@@ -6,42 +6,59 @@ import argparse
 from strategy.futures_bot_strategy import BasicBotStrategy
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', action='store_true', help='for testnet')
+parser.add_argument("-t", action="store_true", help="for testnet")
 args = parser.parse_args()
 
 config = configparser.ConfigParser()
 # reading config file
 config.read("user.cfg")
+symbol = config.get("binance_user_config", "symbol")
+quantity = float(config.get("binance_user_config", "quantity"))
+target_profit_usdt = float(config.get("binance_user_config", "target_profit_usdt"))
+target_loss_usdt = float(config.get("binance_user_config", "target_loss_usdt"))
 
 # use testnet api_key pair for debug
 if args.t:
     api_key = config.get("binance_user_config", "test_api_key")
     api_secret = config.get("binance_user_config", "test_api_secret")
-    client = Client(api_key, api_secret, testnet=True, requests_params={'timeout': 50})
-    strategy=BasicBotStrategy(client)
+    client = Client(api_key, api_secret, testnet=True, requests_params={"timeout": 50})
 else:
     api_key = config.get("binance_user_config", "api_key")
     api_secret = config.get("binance_user_config", "api_secret")
-    client = Client(api_key, api_secret, requests_params={'timeout': 50})
-    strategy=BasicBotStrategy(client)
+    client = Client(api_key, api_secret, requests_params={"timeout": 50})
 
-symbol = config.get("binance_user_config", "symbol")
-quantity = float(config.get("binance_user_config", "quantity"))
-target_profit = float(config.get("binance_user_config", "target_profit"))
-stop_loss_profit = float(config.get("binance_user_config", "stop_loss_profit"))
-target_profit_usdt = float(config.get("binance_user_config", "target_profit_usdt"))
-target_loss_usdt = float(config.get("binance_user_config", "target_loss_usdt"))
+# 获取Futures账户信息
+account_info = client.futures_account()
+# 打印账户总余额
+print("初始总余额:{:.2f} USDT".format(float(account_info["totalWalletBalance"])))
+
+# 使用BasicBotStrategy
+strategy = BasicBotStrategy(client, symbol)
 
 # get the current price for the symbol
 prices = client.get_symbol_ticker(symbol=symbol)
 current_price = float(prices["price"])
-
 notional = quantity * current_price
-min_notional = 100.0
+min_notional = 0
+
+# 获取交易对信息
+info = client.get_exchange_info()
+
+# 在交易对信息中查找特定symbol的信息
+for symbol_info in info['symbols']:
+    if symbol_info['symbol'] == symbol:
+        print(symbol_info)
+        for filter in symbol_info['filters']:
+            if filter['filterType'] == 'NOTIONAL':
+                min_notional = float(filter['minNotional'])
+                print("min_notional:", min_notional)
+                break
+
 print("notional:", notional)
-print("min quantity:", min_notional / current_price)
 if notional < min_notional:
     quantity = min_notional / current_price
+    quantity = round(quantity, 2)
+    print("fixed notional:", quantity * current_price)
 
 leverage = config.get("binance_user_config", "leverage")
 response = client.futures_change_leverage(symbol=symbol, leverage=leverage)
@@ -53,60 +70,30 @@ print(response)
 # response = client.futures_change_position_mode(dualSidePosition=False)
 # print(response)
 
-def clearAllFuturesPosition(client):
-    # clear all positions for testing.
-    positions = client.futures_account()["positions"]
-    for position in positions:
-        symbol_tmp = position["symbol"]
-        qty = float(position["positionAmt"])  # 当前持仓数量
-        if qty != 0:  # 如果持有该合约的仓位不为0，则需要平仓
-            if qty > 0:  # 如果是多头仓位，则需要卖出平仓
-                order = client.futures_create_order(
-                    symbol=symbol_tmp,
-                    side=SIDE_SELL,
-                    type=ORDER_TYPE_MARKET,
-                    quantity=abs(qty),
-                )
-            elif qty < 0:  # 如果是空头仓位，则需要买入平仓
-                order = client.futures_create_order(
-                    symbol=symbol_tmp,
-                    side=SIDE_BUY,
-                    type=ORDER_TYPE_MARKET,
-                    quantity=abs(qty),
-                )
-            print(f"平仓订单已提交：{order}")
-
 print("symbol is:", symbol)
-clearAllFuturesPosition(client)
+# 开始前先取消所有挂单
+strategy.close_all()
 
-# 获取Futures账户信息
-account_info = client.futures_account()
-
-# 打印账户总余额
-print("总余额:", account_info["totalWalletBalance"])
-
-purchase_price = 0
-BUY_FIRST = True
-
-# 通过K线判断是买空还是买多
-klines = client.get_historical_klines(
-        symbol, Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"
-        )
-last_klines = klines[-3:]
-# 比较最早一次和最后一次K线的收盘价
-first_close_price = float(last_klines[0][4])
-last_close_price = float(last_klines[-1][4])
-if last_close_price > first_close_price:
-    print("总体趋势是上涨")
-    BUY_FIRST = True
-else:
-    print("总体趋势是下跌")
-    BUY_FIRST = False
-
-if BUY_FIRST:
-    buy_order = strategy.buy(symbol, quantity)
-else:
-    sell_order = strategy.sell(symbol, quantity)
+#EXPECT_LONG_FIRST = True
+## 通过K线判断是买空还是买多, 后续调整策略
+#klines = client.get_historical_klines(
+#    symbol, Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"
+#)
+#last_klines = klines[-3:]
+## 比较最早一次和最后一次K线的收盘价
+#first_close_price = float(last_klines[0][4])
+#last_close_price = float(last_klines[-1][4])
+#if last_close_price > first_close_price:
+#    print("总体趋势是上涨, 买多")
+#    EXPECT_LONG_FIRST = True
+#else:
+#    print("总体趋势是下跌, 买空")
+#    EXPECT_LONG_FIRST = False
+#
+#if EXPECT_LONG_FIRST:
+#    order = strategy.open_long(quantity)
+#else:
+#    order = strategy.open_short(quantity)
 
 while True:
     print("--------------------------------------------------")
@@ -129,21 +116,22 @@ while True:
         # 打印每个持仓的未实现盈利
         if float(position["positionAmt"]) != 0:  # 筛选出当前有持仓的合约
             print(
-                f"""Symbol: {position['symbol']}, entryPrice: {position['entryPrice']}, markPrice: {position['markPrice']}, Profit: {position['unRealizedProfit']} USDT"""
+                f"""Symbol: {position['symbol']}, entryPrice: {position['entryPrice']},
+                markPrice: {position['markPrice']}, Profit: {position['unRealizedProfit']} USDT
+                """
             )
-            if purchase_price != position["entryPrice"]:
-                purchase_price = float(position["entryPrice"])
-                print(f"purchase_price: {purchase_price}")
 
-    if strategy.should_buy(symbol):
-        # 执行买入操作
-        buy_order = strategy.buy(symbol, quantity)
-        purchase_price = strategy.get_price(symbol)
-        print(f"Bought at {purchase_price}")
+    if strategy.should_open_long():
+        # 执行买多操作
+        order = strategy.open_long(quantity)
+    elif strategy.should_open_short():
+        # 执行买空操作
+        order = strategy.open_short(quantity)
 
-    # if should_sell(purchase_price):
-    if strategy.should_sell_with_unrealized_pnl(total_unrealized_pnl, target_profit_usdt, target_loss_usdt):
-        # 执行卖出操作
-        sell_order = strategy.sell(symbol, quantity)
-        print(f"Sold at {strategy.get_price(symbol)}")
+    if strategy.should_close(
+        total_unrealized_pnl, target_profit_usdt, target_loss_usdt
+    ):
+        # 执行平仓操作
+        sell_order = strategy.close_all()
+        print(f"Sold at {strategy.get_price()}")
     time.sleep(2)
--- strategy/futures_bot_strategy.py
+++ strategy/futures_bot_strategy.py
@@ -2,52 +2,104 @@ from binance.client import Client
 from binance.enums import *
 
 class BasicBotStrategy:
-    def __init__(self, client):
+    def __init__(self, client, symbol):
         self.client = client
+        self.symbol = symbol
 
-    def get_price(self, symbol):
+    def get_price(self):
         # 获取当前市场价格
-        avg_price = self.client.get_avg_price(symbol=symbol)
+        avg_price = self.client.get_avg_price(symbol=self.symbol)
         return float(avg_price["price"])
 
-    def buy(self, symbol, quantity):
-        # 执行买入操作
-        print("buy with quantity:", quantity)
+    def open_long(self, quantity):
+        # 执行做多操作
+        print("open_long with quantity:", quantity)
         order = self.client.futures_create_order(
-            symbol=symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET, quantity=quantity
+            symbol=self.symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET, quantity=quantity
         )
         return order
 
-    def sell(self, symbol, quantity):
-        # 执行卖出操作
+    def open_short(self, quantity):
+        # 执行做空操作
         print("sell with quantity:", quantity)
         order = self.client.futures_create_order(
-            symbol=symbol, side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=quantity
+            symbol=self.symbol,
+            side=SIDE_SELL,
+            type=ORDER_TYPE_MARKET,
+            quantity=quantity,
         )
         return order
 
+    def close_all(self):
+        positions = self.client.futures_account()["positions"]
+        for position in positions:
+            symbol_tmp = position["symbol"]
+            qty = float(position["positionAmt"])  # 当前持仓数量
+            if qty != 0:  # 如果持有该合约的仓位不为0，则需要平仓
+                print("qty is", qty)
+                if qty > 0:  # 如果是多头仓位，则需要卖出平仓
+                    self.client.futures_create_order(
+                        symbol=symbol_tmp,
+                        side=SIDE_SELL,
+                        type=ORDER_TYPE_MARKET,
+                        quantity=abs(qty),
+                    )
+                elif qty < 0:  # 如果是空头仓位，则需要买入平仓
+                    self.client.futures_create_order(
+                        symbol=symbol_tmp,
+                        side=SIDE_BUY,
+                        type=ORDER_TYPE_MARKET,
+                        quantity=abs(qty),
+                    )
+                print("close order with qty:", qty)
 
-    def should_buy(self, symbol):
-        # 这里应该是你的买入逻辑，比如价格上升趋势
-        position_info = self.client.futures_position_information(symbol=symbol)
+    def should_open_long(self):
+        # 如果已经有持仓，则不需要再开仓
+        position_info = self.client.futures_position_information(symbol=self.symbol)
         for position in position_info:
             if float(position["positionAmt"]) != 0:  # 筛选出当前有持仓的合约
                 return False
-        return True
-
-
-    def should_sell(self, symbol, purchase_price, target_profit, stop_loss_profit):
-        # 当前价格达到目标盈利时返回True
-        current_price = self.get_price(symbol)
-        current_profit = (current_price - purchase_price) / purchase_price * 100
-        print(f"current profit: {current_profit:.2f}%")
-        if current_profit > target_profit or current_profit < stop_loss_profit:
+        klines = self.client.get_historical_klines(
+                self.symbol, Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"
+                )
+        last_klines = klines[-3:]
+        # 比较最早一次和最后一次K线的收盘价
+        first_close_price = float(last_klines[0][4])
+        last_close_price = float(last_klines[-1][4])
+        if last_close_price > first_close_price:
+            print("总体趋势是上涨, 买多")
             return True
         else:
             return False
 
-    def should_sell_with_unrealized_pnl(self, total_unrealized_pnl, target_profit_usdt, target_loss_usdt):
-        if float(total_unrealized_pnl) > target_profit_usdt or float(total_unrealized_pnl) < target_loss_usdt:
+    def should_open_short(self):
+        # 如果已经有持仓，则不需要再开仓
+        position_info = self.client.futures_position_information(symbol=self.symbol)
+        for position in position_info:
+            if float(position["positionAmt"]) != 0:  # 筛选出当前有持仓的合约
+                return False
+        klines = self.client.get_historical_klines(
+                self.symbol, Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"
+                )
+        last_klines = klines[-3:]
+        # 比较最早一次和最后一次K线的收盘价
+        first_close_price = float(last_klines[0][4])
+        last_close_price = float(last_klines[-1][4])
+        if last_close_price > first_close_price:
+            return False
+        else:
+            print("总体趋势是下跌, 买空")
+            return True
+
+    # 判断是否需要平仓, 当前的未实现盈亏超过目标盈利或者亏损超过目标亏损, 需要平仓
+    # total_unrealized_pnl: 总的未实现盈亏
+    # target_profit_usdt: 目标盈利
+    # target_loss_usdt: 目标亏损
+    def should_close(self, total_unrealized_pnl, target_profit_usdt, target_loss_usdt):
+        if (
+            float(total_unrealized_pnl) > target_profit_usdt
+            or float(total_unrealized_pnl) < target_loss_usdt
+        ):
             return True
         else:
             return False
